fd2eefa7dc25041fec51c1732df736f3bb660220
Rah Emil <013131@mail.ru>
2022-01-14 17:53:22 +0300

    fix ESlint & v 1.0.2

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,16 @@ module.exports = {
         "es2021": true
     },
     "extends": [
-        "airbnb",
+        "airbnb-base",
         "eslint:recommended",
         "plugin:vue/essential",
-        "plugin:@typescript-eslint/recommended"
     ],
     "parserOptions": {
         "ecmaVersion": 13,
-        "parser": "@typescript-eslint/parser",
         "sourceType": "module"
     },
     "plugins": [
         "vue",
-        "@typescript-eslint"
     ],
     "rules": {
     }
