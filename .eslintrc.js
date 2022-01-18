7f8303e06aec61be332f257746a97874b29e2021
Mikhail Sitnikov <67879032+justMikhail@users.noreply.github.com>
2022-01-18 14:04:18 +0300

    Eslint rules update. (Add "eslint-config-airbnb-typescript") (#1)
    Co-authored-by: Rah Emil <013131@mail.ru>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,39 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "airbnb-base",
-        "eslint:recommended",
-        "plugin:vue/essential",
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:@typescript-eslint/recommended',
+  ],
+
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
     ],
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-    ],
-    "rules": {
-    }
+    'import/prefer-default-export': 'off',
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+  },
 };
